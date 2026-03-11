4ccec732a770c2979d6b0c6a6b9427289b3a7cd9
Karl Cayme <kcayme@gmail.com>
2026-03-11 15:51:55 +0800

    feat: update mason highlight

--- lua/bearded-arc/groups/mason.lua
+++ lua/bearded-arc/groups/mason.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.get(c, opts)
   return {
-    MasonHeader = { fg = c.bg, bg = c.red },
+    MasonHeader = { fg = c.bg, bg = c.cyan },
     MasonHighlight = { fg = c.blue },
     MasonHighlightBlock = { fg = c.bg, bg = c.green },
     MasonHighlightBlockBold = { link = "MasonHighlightBlock" },
