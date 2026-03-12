39dbbcda5af352715cc29bd2f54b2bddf503d134
Karl Cayme <kcayme@gmail.com>
2026-03-13 00:57:40 +0800

    feat: update markview hl

--- lua/bearded-arc/groups/markview.lua
+++ lua/bearded-arc/groups/markview.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.get(c, opts)
   return {
-    ["@markup.heading.1.markdown"] = { fg = c.red, bold = true },
+    ["@markup.heading.1.markdown"] = { fg = c.magenta, bold = true },
     ["@markup.heading.2.markdown"] = { fg = c.orange, bold = true },
     ["@markup.heading.3.markdown"] = { fg = c.yellow, bold = true },
     ["@markup.heading.4.markdown"] = { fg = c.green, bold = true },
